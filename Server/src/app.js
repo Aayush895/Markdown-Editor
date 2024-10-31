e59d5dc7e1683ea27aeaf705595eec9c6a8f9d04
Aayush895 <aayush895@gmail.com>
2024-10-31 19:37:48 +0530

    Added healthcheck routes and logger files and made some changes in the readme file

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,5 +1,34 @@
 import express from "express";
+import morgan from "morgan";
+import logger from "../logs/logger.js";
 
 const app = express();
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// routes import
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+// routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
