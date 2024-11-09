95901674778a76f14aaa2951149b666e804485fc
Aayush895 <aayush895@gmail.com>
2024-11-09 20:02:40 +0530

    Created the config for user registeration route

---
+++ Server/src/routes/v1/usersRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router()
+
+// Below route is used for resgistering a user
+// router.post("/register", regiterUser)
+
+export default router
--- Server/src/routes/v1/v1Routes.js
+++ Server/src/routes/v1/v1Routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import healthcheckRouter from './healthcheckroutes.js'
+import healthcheckRouter from "./healthcheckroutes.js";
+import usersRouter from "./usersRoute.js";
 
 const router = express.Router();
 
 router.use("/healthcheck", healthcheckRouter);
+router.use("/users", usersRouter);
 
 export default router;
