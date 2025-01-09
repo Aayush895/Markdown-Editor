47bd1758b8fe917e6e9164ae11212265afd8ddec
Aayush895 <aayush895@gmail.com>
2025-01-09 11:32:05 +0530

    Changed Cors configuration

--- Server/src/index.js
+++ Server/src/index.js
@@ -17,7 +17,14 @@ app.use(
   })
 );
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(
   morgan(morganFormat, {
