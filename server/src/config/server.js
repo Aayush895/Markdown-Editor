0670b3bd27dd12b366a270f8d53d08260413094f
Aayush895 <aayush895@gmail.com>
2025-06-13 17:40:47 +0530

    Modified cors configuration

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -6,9 +6,30 @@ import { documentRouter } from '../routes/documentRoutes/documents.routes.js';
 export const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://my-notes-app.netlify.app',
+];
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//   })
+// );
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like curl or Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // Include if using cookies or headers requiring credentials
   })
 );
 
